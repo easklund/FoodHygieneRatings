ac075c99e5f1e7e9c9aae75a4a5a0a7dcbbe3973
easklund <dat14eas@student.lu.se>
2019-03-21 19:52:04 +0000

    Added a detailed view were I'm showing the name, type and ID in a new activity

--- app/src/main/java/com/example/foodhygieneratings/Details.java
+++ app/src/main/java/com/example/foodhygieneratings/Details.java
@@ -1,19 +1,19 @@
 package com.example.foodhygieneratings;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
@@ -22,48 +22,32 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Result extends AppCompatActivity {
-    private String TAG = "Result";
-
-    private ArrayList<Establishment> establishments =  new ArrayList<Establishment>();
-    public ArrayAdapter establishAdpt;
+public class Details extends AppCompatActivity {
+    private String TAG = "Details";
+    private int FHRSID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen_result);
+        setContentView(R.layout.screen_details);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Result from you search");
 
-        establishAdpt = new ArrayAdapter(this,android.R.layout.simple_selectable_list_item, establishments);
-        ListView listview = (ListView)findViewById(R.id.ListViewResult);
-        listview.setAdapter(establishAdpt);
-
-        onRequestEstablishment(listview);
-
-        final AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Establishment clickedItem = (Establishment) establishAdpt.getItem(position);
-                Intent intent = new Intent(Result.this, Details.class);
-                intent.putExtra("FHRSID", clickedItem.getFHRSID());
-                startActivity(intent);
-            }
-        };
-        listview.setOnItemClickListener(itemClickListener);
+        FHRSID = getIntent().getIntExtra("FHRSID",0);
+        onRequestEstablishment();
+        
     }
 
-
-    public void onRequestEstablishment(View view){
+    public void onRequestEstablishment(){
         Log.d(TAG, "onRequestEstablishment: ");
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-        final String allEstablishments = "http://api.ratings.food.gov.uk/Establishments/basic";
-        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, allEstablishments, null,
+        final String establishment = "http://api.ratings.food.gov.uk/Establishments/";
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, establishment + FHRSID, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
@@ -86,30 +70,42 @@ public class Result extends AppCompatActivity {
                 return params;
             }
         };
+        getRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 800000;
+            }
+            @Override
+            public int getCurrentRetryCount() {
+                return 800000;
+            }
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
         requestQueue.add(getRequest);
     }
 
     public void estabishmentsList(JSONObject items) throws JSONException {
         Log.d(TAG, "estabishmentsList: started");
-        establishments.clear();
-        Log.d(TAG, "estabishmentsList: cleared");
-        JSONArray jArray = items.getJSONArray("establishments");
-        Log.d(TAG, "estabishmentsList: before try");
+        TextView name = findViewById(R.id.NameView);
+        TextView type = findViewById(R.id.TypeView);
+        TextView id = findViewById(R.id.IDView);
+        Log.d(TAG, "estabishmentsList: " + items.getString("BusinessName"));
         try{
             Log.d(TAG, "estabishmentsList: in Try");
-            for(int i = 0; i < jArray.length(); i++){
-                JSONObject jo = jArray.getJSONObject(i);
-                JSONArray ja = jo.getJSONArray("links");
-                HashMap<String, String> hm = new HashMap<String, String>();
-                for(int j = 0; j < ja.length(); j++){
-                    hm.put(ja.getJSONObject(0).getString("rel"), ja.getJSONObject(0).getString("href"));
-                }
-                establishments.add(new Establishment(jo.getInt("FHRSID"), jo.getString("LocalAuthorityBusinessID"), jo.getString("BusinessName"), jo.getString("BusinessType"), jo.getString("RatingValue"), jo.getString("RatingDate"), hm));
-            }
+            Log.d(TAG, "estabishmentsList: " + items.getString("BusinessName"));
+            Log.d(TAG, "estabishmentsList: " + items.getString("BusinessType"));
+            Log.d(TAG, "estabishmentsList: " + items.getInt("FHRSID"));
+            name.setText("Name: " + items.getString("BusinessName"));
+            type.setText("Type: " + items.getString("BusinessType"));
+            String s = "ID: " + Integer.toString(items.getInt("FHRSID"));
+            id.setText(s);
+
         }catch (JSONException err){
             Log.d(TAG, "estabishmentsList, error: " + err);
         }
-        establishAdpt.notifyDataSetChanged();
         Log.d(TAG, "estabishmentsList: done");
     }
 }
