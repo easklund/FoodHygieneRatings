b1debddb262e52f32c74aac84cfee37c58943c85
easklund <dat14eas@student.lu.se>
2019-03-26 15:26:33 +0000

    The database work but not implemented in the interface

---
+++ app/src/main/java/com/example/foodhygieneratings/Fave.java
@@ -0,0 +1,13 @@
+package com.example.foodhygieneratings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_fave);
+    }
+}
